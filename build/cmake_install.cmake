4094dffff5968f5395417c35be7350b244c75467
peppypenguins <adamkherring@gmail.com>
2025-07-30 19:13:33 -0500

    Mostly just added some vscode capability

--- build/cmake_install.cmake
+++ build/cmake_install.cmake
@@ -1,4 +1,4 @@
-# Install script for directory: C:/Users/adamk/OneDrive/Documents/GitHub/AHasm
+# Install script for directory: C:/Users/Adam/Documents/GitHub/AHasm
 
 # Set the install prefix
 if(NOT DEFINED CMAKE_INSTALL_PREFIX)
@@ -34,7 +34,7 @@ endif()
 
 # Set path to fallback-tool for dependency-resolution.
 if(NOT DEFINED CMAKE_OBJDUMP)
-  set(CMAKE_OBJDUMP "C:/msys64/mingw64/bin/objdump.exe")
+  set(CMAKE_OBJDUMP "C:/msys64/ucrt64/bin/objdump.exe")
 endif()
 
 if(CMAKE_INSTALL_COMPONENT)
@@ -52,6 +52,6 @@ endif()
 if(NOT CMAKE_INSTALL_LOCAL_ONLY)
   string(REPLACE ";" "\n" CMAKE_INSTALL_MANIFEST_CONTENT
        "${CMAKE_INSTALL_MANIFEST_FILES}")
-  file(WRITE "C:/Users/adamk/OneDrive/Documents/GitHub/AHasm/build/${CMAKE_INSTALL_MANIFEST}"
+  file(WRITE "C:/Users/Adam/Documents/GitHub/AHasm/build/${CMAKE_INSTALL_MANIFEST}"
      "${CMAKE_INSTALL_MANIFEST_CONTENT}")
 endif()
--- src/main.c
+++ src/main.c
@@ -1,15 +1,15 @@
 #include "main.h"
 
 int main(){
-char inputFilePath[64];
-char outputFilePath[64]; 
+    char inputFilePath[64];
+    char outputFilePath[64]; 
 
-obtainFilePath(inputFilePath, outputFilePath, 64);
+    obtainFilePath(inputFilePath, outputFilePath, 64);
 
-printf("Entered input file path, max length 64: %s\n",inputFilePath);
-printf("Entered output file path, max length 64: %s\n", outputFilePath);
+    printf("Entered input file path, max length 64: %s\n",inputFilePath);
+    printf("Entered output file path, max length 64: %s\n", outputFilePath);
 
-assemble(inputFilePath,outputFilePath);
-printf("Successfully assembled given program");
+    assemble(inputFilePath,outputFilePath);
+    printf("Successfully assembled given program");
     return 0;
 }
